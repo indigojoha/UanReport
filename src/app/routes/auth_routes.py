b8162315c5ecf2adc0f01a1fcf3ca87c6e52802e
indigoJoha <indigoUan@gmail.com>
2025-08-07 06:02:36 +0200

    Custom make_response
    Replaces Flask's jsonify with a custom make_json_response function in player_connection_permission for more explicit control over response headers and content length. This change ensures consistent JSON responses and may improve compatibility with clients expecting specific headers.

--- src/app/routes/auth_routes.py
+++ src/app/routes/auth_routes.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, make_response
 from app.services import USERS, PHANDLER
+import json
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/api')
 
@@ -17,23 +18,29 @@ def try_login():
 def player_connection_permission():
 	steamID = request.args.get('steamid')
 
+	def make_json_response(payload: dict):
+		resp = make_response(json.dumps(payload))
+		resp.headers['Content-Type'] = 'application/json'
+		resp.headers['Content-Length'] = str(len(resp.get_data()))
+		return resp
+
 	suspension = PHANDLER.get_suspension(steamID)
 	if suspension:
 		if suspension['days'] == 0:
-			return jsonify({'result': 'banned', 'extra': {'reason': suspension['reason'], 'date': suspension['date']}}), 200
+			return make_json_response({'result': 'banned', 'extra': {'reason': suspension['reason'], 'date': suspension['date']}})
 		else:
 			suspended_date = datetime.fromisoformat(suspension['date']).date()
 			days_passed = (date.today() - suspended_date).days
 			if days_passed < suspension['days']:
-				return jsonify({'result': 'suspended', 'extra': {'days_remaining': suspension['days'] - days_passed, 'reason': suspension['reason'], 'date': suspension['date']}}), 200
+				return make_json_response({'result': 'suspended', 'extra': {'days_remaining': suspension['days'] - days_passed, 'reason': suspension['reason'], 'date': suspension['date']}})
 			else:
 				PHANDLER.lift_suspension(steamID)
-				return jsonify({'result': 'lifted', 'extra': None}), 200
+				return make_json_response({'result': 'lifted', 'extra': None})
 
 	warning = PHANDLER.get_warning(steamID)
 	if warning:
-		result = jsonify({'result': 'warned', 'extra': {'reason': warning['reason'], 'date': warning['date']}})
+		result = make_json_response({'result': 'warned', 'extra': {'reason': warning['reason'], 'date': warning['date']}})
 		PHANDLER.lift_warning(steamID)
 		return result, 200
 
-	return jsonify({'result': 'permitted', 'extra': None}), 200
+	return make_json_response({'result': 'permitted', 'extra': None})
